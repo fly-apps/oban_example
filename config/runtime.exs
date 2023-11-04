cb00cc76592809541e9f205e3f14eb2cedb14ce1
Matthew Lehner <matthew@mpl.io>
2023-11-03 17:52:04 -0700

    Ensure only nodes in primary region process jobs

--- config/runtime.exs
+++ config/runtime.exs
@@ -47,6 +47,13 @@ if config_env() == :prod do
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     socket_options: maybe_ipv6
 
+
+  unless Fly.RPC.is_primary? do
+    config :oban_example, Oban,
+      queues: false,
+      plugins: false,
+      peer: false
+  end
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
   # want to use a different value for prod and you most likely don't want
